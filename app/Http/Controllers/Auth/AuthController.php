99558c4c3251638e14378721361c4f97ce747aa6
Kohei Komachi <kohei.komachi@gmail.com>
2017-08-18 04:51:10 +0000

    user login

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -62,4 +62,6 @@ class AuthController extends Controller
             'password' => bcrypt($data['password']),
         ]);
     }
+    protected $redirectTo = '/';        // 追加
+    protected $loginPath = '/login';    // 追加
 }
